2f413d30e766ef523fa553ad2be700a413cc52f5
Jonathan <jonathanolivataiwan@gmail.com>
2019-05-23 20:27:16 +0800

    rubocop

--- app/services/get_all_notes.rb
+++ app/services/get_all_notes.rb
@@ -11,10 +11,10 @@ module LastWillFile
     end
 
     def call(current_account)
-    response = HTTP.auth("Bearer #{current_account.auth_token}")
-                   .get("#{@config.API_URL}/notes")
+      response = HTTP.auth("Bearer #{current_account.auth_token}")
+                     .get("#{@config.API_URL}/notes")
 
-    response.code == 200 ? response.parse['data'] : nil
+      response.code == 200 ? response.parse['data'] : nil
     end
   end
 end
