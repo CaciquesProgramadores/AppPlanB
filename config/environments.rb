fcb5e0ffb77c814995b9bea89dbf8b5229fae6ab
esamgel <esamgel@gmail.com>
2019-06-19 19:58:23 +0800

    fix sslforcer by adding SSL require2

--- config/environments.rb
+++ config/environments.rb
@@ -26,7 +26,7 @@ module LastWillFile
     end
 
     configure :production do
-      #use Rack::SSL
+      use Rack::SSL
       use Rack::SslEnforcer #, hsts: true
 
       use Rack::Session::Redis,
