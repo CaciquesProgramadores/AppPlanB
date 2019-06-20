15efe4db68871d83249454455befc8d1732d025f
Jonathan <jonathanolivataiwan@gmail.com>
2019-06-20 19:24:14 +0800

    trigger death

--- app/controllers/account.rb
+++ app/controllers/account.rb
@@ -43,6 +43,20 @@ module LastWillFile
             "#{App.config.APP_URL}/auth/register/#{registration_token}"
           )
         end
+
+        # GET api/v1/accounts/existences
+        routing.on('existences') do
+          routing.get do
+            existences = GetExistences.new(App.config).call(
+              @current_account
+            )
+
+            view :existences, locals: { existences: existences  }
+          rescue GetExistences::NotFoundError => e
+            flash[:error] = e.message
+            routing.redirect '/'
+          end
+        end
       end
     end
   end
---
+++ app/services/get_existences.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'http'
+
+module LastWillFile
+  # Returns all notes belonging to an account
+  class GetExistences
+    # Error for cannot find existences
+    class NotFoundError < StandardError
+      def message
+        'We could not find that existences'
+      end
+    end
+
+    def initialize(config)
+      @config = config
+    end
+
+    def call(current_account)
+      response = HTTP.auth("Bearer #{current_account.auth_token}")
+                   .get("#{@config.API_URL}/accounts/existences")
+
+      existences = response.parse['data']
+
+      puts existences
+
+      existences
+    end
+  end
+end
