bc4383d99647be3ffd0ca955e25c824b2f9e70d9
Jonathan <jonathanolivataiwan@gmail.com>
2019-06-23 17:07:58 +0800

    death trigger

---
+++ app/controllers/existences.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'roda'
+require_relative './app'
+require 'pry'
+
+module LastWillFile
+  # Web controller for LastWillFile API
+  class App < Roda
+    route('existences') do |routing|
+      routing.redirect '/auth/login' unless @current_account.logged_in?
+
+      # GET /existences/
+      routing.get do
+        existences = GetExistences.new(App.config).call(
+          @current_account
+        )
+
+        view :existences, locals: { existences: existences  }
+      rescue GetExistences::NotFoundError => e
+        flash[:error] = e.message
+        routing.redirect '/'
+      end
+    end
+  end
+end
