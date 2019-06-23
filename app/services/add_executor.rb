1a46e421a21e7c3c47ce5b2183116a7c785fea6a
Jonathan <jonathanolivataiwan@gmail.com>
2019-06-23 22:50:01 +0800

    fixing problems

--- app/services/add_executor.rb
+++ app/services/add_executor.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 require 'pry'
-# Service to add collaborator to project
+# Service to add executors to notes
 class AddExecutor
     class ExecutorNotAdded < StandardError; end
-  
+
     def initialize(config)
       @config = config
     end
-  
+
     def api_url
       @config.API_URL
     end
-  
+
     def call(current_account:, executor:, project_id:)
       response = HTTP
         .auth("Bearer #{current_account.auth_token}")
-        .put("#{api_url}/notes/#{project_id}/collaborators",
-            json: { email: collaborator[:email] })
+        .put("#{api_url}/notes/#{project_id}/executors",
+            json: { email: executor[:email] })
       #binding.pry
       raise ExecutorNotAdded unless response.code == 200
     end
